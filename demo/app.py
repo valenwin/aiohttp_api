bf45193cb8effb0201f7291b710ca6ff206f0296
valenwin <tina.lysenok@gmail.com>
2020-11-16 17:11:15 +0200

    added configuration settings

--- demo/app.py
+++ demo/app.py
@@ -1,8 +1,10 @@
 from aiohttp import web
+
 from .routes import setup_routes
 
 
-async def create_app():
+async def create_app(config: dict):
     app = web.Application()
+    app['config'] = config
     setup_routes(app)
     return app
